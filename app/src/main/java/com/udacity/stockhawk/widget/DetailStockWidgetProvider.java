e1fa82e7a84c27c2c22cbc5947f648eb6c467668
CarolineClark <carolinejaneclark@gmail.com>
2017-04-16 15:28:38 +0100

    Update widget

--- app/src/main/java/com/udacity/stockhawk/widget/DetailStockWidgetProvider.java
+++ app/src/main/java/com/udacity/stockhawk/widget/DetailStockWidgetProvider.java
@@ -3,8 +3,10 @@ package com.udacity.stockhawk.widget;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v4.app.TaskStackBuilder;
 import android.widget.RemoteViews;
 
@@ -12,6 +14,8 @@ import com.udacity.stockhawk.R;
 import com.udacity.stockhawk.ui.MainActivity;
 
 public class DetailStockWidgetProvider extends AppWidgetProvider {
+
+    private String ACTION_DATA_UPDATED = "com.udacity.stockhawk.ACTION_DATA_UPDATED";
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -21,6 +25,7 @@ public class DetailStockWidgetProvider extends AppWidgetProvider {
             views.setRemoteAdapter(R.id.widget_list, new Intent(context, DetailWidgetRemoteViewsService.class));
 
             // TODO: this should be the DetailActivity, when this exists.
+            // TODO: need to pass the history to the detailactivity
             Intent clickIntentTemplate = new Intent(context, MainActivity.class);
             PendingIntent clickPendingIntentTemplate = TaskStackBuilder.create(context)
                     .addNextIntentWithParentStack(clickIntentTemplate)
@@ -28,6 +33,16 @@ public class DetailStockWidgetProvider extends AppWidgetProvider {
             views.setPendingIntentTemplate(R.id.widget_list, clickPendingIntentTemplate);
             views.setEmptyView(R.id.widget_list, R.id.widget_empty);
             appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
+    @Override
+    public void onReceive(@NonNull Context context, @NonNull Intent intent) {
+        super.onReceive(context, intent);
+        if (ACTION_DATA_UPDATED.equals(intent.getAction())) {
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(context, getClass()));
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_list);
         }
     }
 }
